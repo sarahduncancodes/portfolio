68a7a779733eef8d60da08fccc3177be1664e84a
unknown <sarahduncancodes@gmail.com>
2023-01-10 11:14:48 +0200

    add html package and view

--- app.js
+++ app.js
@@ -3,9 +3,25 @@
 var express = require("express"); 
 var app = express();
 
+//add express handlebars
+var exphbs = require("express-handlebars");
+
+//add path
+var path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ".hbs");
+app.engine(".hbs", exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: false
+}));
+
+
+
+//this is the homepage
 app.get("/", function(req, res){
-    res.send("Hello uncle Nick! Index route is working! :)");
-}); //this is the homepage
+    res.render("home.hbs");
+}); 
 
 
 //set the port to listen on
